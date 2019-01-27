8a47037e6bdff237b362e166d24bef8814b9c439
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2019-01-27 21:29:10 +0330

    remove toast

--- app/src/main/java/projects/com/amirahmadadibi/myapplication/MainActivity.java
+++ app/src/main/java/projects/com/amirahmadadibi/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package projects.com.amirahmadadibi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+
     }
 }
